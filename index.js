7104684f32730aced2ca4d188e17f9dbbdd9c5f4
Cgard5424 <cgardner858@gmail.com>
2021-12-31 15:33:14 -0500

    Finished exercise 3.4

--- index.js
+++ index.js
@@ -1,40 +1,66 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  { 
+  "id": 1,
+  "name": "Arto Hellas", 
+  "number": "040-123456"
+  },
+  { 
+  "id": 2,
+  "name": "Ada Lovelace", 
+  "number": "39-44-5323523"
+  },
+  { 
+  "id": 3,
+  "name": "Dan Abramov", 
+  "number": "12-43-234345"
+  },
+  { 
+  "id": 4,
+  "name": "Mary Poppendieck", 
+  "number": "39-23-6423122"
+  }
 ]
 
 app.get('/', (request, response) => {
-    response.send(`<h1>Hello, World!</h1>`)
+  response.send(`<h1>Hello, World!</h1>`)
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const num_persons = persons.length
+  const date = Date()
+  response.send(`Phonebook has info for ${num_persons} people<br>${date}`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
