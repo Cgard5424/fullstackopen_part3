4251cbfb40f56c2d94f9dce0043518cae8ea6f64
Cgard5424 <cgardner858@gmail.com>
2022-01-01 17:25:50 -0500

    Added frontend build directory

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ npm start
 OR 
 
 npm run dev
+
+Heroku app: https://vast-badlands-24674.herokuapp.com/
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('type', function (req, res) {
   if (req.method === 'POST') {
